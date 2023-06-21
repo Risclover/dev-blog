a17300b561c8473c7db2c8b668911b01316c1483
Risclover <sara091592@gmail.com>
2023-06-21 13:06:27 -0700

    Updated .gitignore

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "Risclover", // Usually your GitHub org/user name.
   projectName: "dev-blog", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
